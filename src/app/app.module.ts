ff74c39ca7fe6289bfa349d04f0e54e57416b5a7
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-05-27 17:00:56 +0530

    sign in added text input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestingComponent } from './testing/testing.component';
 import { SignInFormComponent } from './sign-in-form/sign-in-form.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SignInFormComponent } from './sign-in-form/sign-in-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
