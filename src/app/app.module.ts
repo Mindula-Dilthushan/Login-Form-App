47b5fa007443dfde6b2bea318c3d7013250e3913
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-05-27 16:23:47 +0530

    sign in form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestingComponent } from './testing/testing.component';
+import { SignInFormComponent } from './sign-in-form/sign-in-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestingComponent,
+    SignInFormComponent
   ],
   imports: [
     BrowserModule,
